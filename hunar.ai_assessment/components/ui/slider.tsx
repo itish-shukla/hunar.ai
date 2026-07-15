7a2c64d23636c6b412a720575a50bc9455faaee6
Itish Raj Shukla <itish2606@gmail.com>
2026-07-15 11:24:02 +0530

    added types library

--- hunar.ai_assessment/components/ui/slider.tsx
+++ hunar.ai_assessment/components/ui/slider.tsx
@@ -34,7 +34,7 @@ function Slider({
         >
           <SliderPrimitive.Indicator
             data-slot="slider-range"
-            className="bg-primary select-none data-horizontal:h-full data-vertical:w-full"
+            clas
           />
         </SliderPrimitive.Track>
         {Array.from({ length: _values.length }, (_, index) => (
